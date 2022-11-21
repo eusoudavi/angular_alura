1a49c9a54f38044f676d85694c883ca43288182f
davi.visintainer <davi.visintainer@placeti.com.br>
2022-11-21 18:08:53 -0300

    refatorando última atualização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
-  valor: number | undefined;
-  destino: number | undefined;
+  transferencia: any;
 
 
   transferir($event: any){
     console.log($event);
-    this.valor = $event.valor;
-    this.destino = $event.destino;
+    this.transferencia = $event;
   }
 }
