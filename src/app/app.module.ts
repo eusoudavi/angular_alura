494b67797759946136eb7a27553e75c309df9f0a
davi.visintainer <davi.visintainer@placeti.com.br>
2022-11-23 12:45:05 -0300

    Criando rotas de acesso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,13 @@ import { FormsModule } from '@angular/forms';
 import { ExtratoComponent } from './extrato/extrato.component';
 import { registerLocaleData } from '@angular/common';
 import localePt from "@angular/common/locales/pt";
+import { AppRoutingModule } from './app-routing.module';
 
 registerLocaleData(localePt, "pt");
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NovaTransferenciaComponent,
-    ExtratoComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent, NovaTransferenciaComponent, ExtratoComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
   providers: [
     { provide: LOCALE_ID, useValue: "pt" },
     {
